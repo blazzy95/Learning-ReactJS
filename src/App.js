e552e3bb6e44513353d9a07506191b96d5c1e087
Jitendra Selvam <jitendra3195@gmail.com>
2017-12-26 15:32:58 -0500

    removed Radium

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
-
 class App extends Component {
     state = {
         persons: [
@@ -48,10 +46,6 @@ class App extends Component {
             border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer',
-			':hover':{
-				backgroundColor:'lightgreen',
-				color:'black'
-			}
         };
 
         let persons = null;
@@ -72,10 +66,6 @@ class App extends Component {
             );
 
 		style.backgroundColor = 'red';
-		style[':hover'] = {
-			backgroundColor:'salmon',
-			color:'black'
-		};
         }
 
 		const classes = [];
@@ -100,4 +90,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
